654e58e867122086146fb56ad5b898a5c3df9f9e
Lærke Høj Lange <44491977+hoejlange@users.noreply.github.com>
2020-04-29 20:21:26 +0200

    fractal build

--- push2ghpages.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-if [ -z "$1" ]
-then
-  echo "Which folder do you want to deploy to GitHub Pages?"
-  echo "(try sh push2ghpages.sh build)"
-  exit 1
-fi
-git subtree push --prefix $1 origin gh-pages
